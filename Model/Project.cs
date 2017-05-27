f4ff76e65641511e51fa1c4aabb663265acded69
天 谢 <jsjtxietian@outlook.com>
2017-05-28 01:28:41 +0800

    change table a lot one to many many to many still need to deal with InverseProperty

--- Model/Project.cs
+++ Model/Project.cs
@@ -1,21 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.ClientServices.Providers;
 
 namespace Backend.Model
 {
     public class Project
     {
+        public Project()
+        {
+            Progresses = new List<Progress>();
+            Schedules = new List<Schedule>();
+            Users = new List<User>();
+            Files = new List<File>();
+            ProjectOperations = new List<ProjectOperation>();
+        }
+        
         public int Id { get; set; }
 
         [Required]
-        [StringLength(20)]
+        [MaxLength(20)]
         public string Name { get; set; }
 
         [Required]
-        [StringLength(100)]
+        [MaxLength(100)]
         public string Description { get; set; }
 
-        public int OwnerId { get; set; }
-        public virtual User Owner { get; set; }
+       // public int UserId { get; set; }//创建者
+        //public User User { get; set; }
+        
+        public virtual ICollection<Progress> Progresses { get; set; }
+        public virtual ICollection<Schedule> Schedules { get; set; } 
+        public virtual ICollection<User> Users { get; set; }
+        public virtual ICollection<File> Files { get; set; }
+        public virtual ICollection<ProjectOperation> ProjectOperations { get; set; }
+        
     }
 }
