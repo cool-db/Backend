1b7864d4c486bc47e51bcbe20c086a8d56c5b534
Lin <184628769@qq.com>
2017-07-10 16:18:39 +0800

    complete schdule

--- Biz/ScheduleBiz.cs
+++ Biz/ScheduleBiz.cs
@@ -35,7 +35,7 @@ namespace Backend.Biz
                     var queryUser = context.Users.Where(user => user.Id == id);
                     
                     if (!queryUser.Any())
-                        return Helper.Error(401, "用户" + participatorId + "不存在");
+                        return Helper.Error(404, "用户" + participatorId + "不存在");
                     
                     users.Add(queryUser.Single());
                     
@@ -49,7 +49,7 @@ namespace Backend.Biz
 
                 var queryProject = context.Projects.Where(project => project.Id == projectId);
                 if (!queryProject.Any())
-                    return Helper.Error(401, "项目不存在");
+                    return Helper.Error(401, "日程所属项目不存在");
                 
 
                 //var users = participatorIds.Select(participatorId => context.Users.Single(user => user.Id == int.Parse(participatorId))).ToList();
@@ -98,12 +98,12 @@ namespace Backend.Biz
 
                 var querySchedule = context.Schedules.Where(schedule => schedule.Id == scheduleId);
                 if (!querySchedule.Any())
-                    return Helper.Error(401, "删除日程不存在");
+                    return Helper.Error(404, "删除日程不存在");
                 var theSchedule = querySchedule.Single();
 
                 var queryProject = context.Projects.Where(project => project.Id == querySchedule.Single().Id);
                 if (!queryProject.Any())
-                    return Helper.Error(401, "删除日程所属项目不存在");
+                    return Helper.Error(404, "删除日程所属项目不存在");
                 
                 if(!Helper.CheckPermission(queryProject.Single().Id, operatorId, true, OperationType.DELETE))
                     return Helper.Error(401, "用户无操作权限");
@@ -135,40 +135,21 @@ namespace Backend.Biz
                 var endTime = DateTime.Parse(body["endTime"].ToString());
                 var repeatDaily = bool.Parse(body["repeatDaily"].ToString());
                 var repeatWeekly = bool.Parse(body["repeatWeekly"].ToString()); 
-                var participatorIds = JArray.Parse(body["participatorIds"].ToString());
                 var projectId = int.Parse(body["projectId"].ToString());
 
                 var querySchedule = context.Schedules.Where(schedule => schedule.Id == scheduleId);
                 if (!querySchedule.Any())
-                    return Helper.Error(401, "更新日程不存在");
+                    return Helper.Error(404, "更新日程不存在");
 
                 var queryProject = context.Projects.Where(project => project.Id == projectId);
                 if (!queryProject.Any())
-                    return Helper.Error(401, "项目不存在");
-                
-                //var users = new List<User>();
-                var ids = new List<int>();
-                foreach (var participatorId in participatorIds)
-                { 
-                    var id = int.Parse(participatorId.ToString());
-                    var queryUser = context.Users.Where(user => user.Id == id);
-                    
-                    if (!queryUser.Any())
-                        return Helper.Error(401, "用户" + participatorId + "不存在");
-                    
-                    //users.Add(queryUser.Single());
-                    
-                    ids.Add(id);
-                 
-                }
+                    return Helper.Error(404, "更新日程所属项目不存在");
                 
                 if(!Helper.CheckPermission(projectId, operatorId, true, OperationType.PUT))
                     return Helper.Error(401, "用户无操作权限");
                 
                 var theSchedule = querySchedule.Single();
-                
                 var theProject = queryProject.Single();
-                
 
                 theSchedule.Content = scheduleContent;
                 theSchedule.EndTime = endTime;
@@ -179,7 +160,6 @@ namespace Backend.Biz
                 theSchedule.ProjectId = projectId;
                 theSchedule.RepeatDaily = repeatDaily;
                 theSchedule.RepeatWeekly = repeatWeekly;
-                //theSchedule.Users = users;
 
                 context.SaveChanges();
 
@@ -193,20 +173,15 @@ namespace Backend.Biz
                     endTime = theSchedule.EndTime,
                     repeatDaily = theSchedule.RepeatDaily,
                     repeatWeekly = theSchedule.RepeatWeekly,
-                    participatorIds = ids,
                     code = 200
                 };
             }
         }
         
-        public static object GetSchedule(object json)
-        {
-            var body = Helper.DecodeToObject(json);
-            
+        public static object GetSchedule(int scheduleId)
+        { 
             using (var context = new BackendContext())
             {
-                var scheduleId = int.Parse(body["scheduleId"].ToString());
-
                 var querySchedule = context.Schedules.Where(schedule => schedule.Id == scheduleId);
 
                 if (!querySchedule.Any())
@@ -216,26 +191,25 @@ namespace Backend.Biz
 
                 var dataList = new List<object>();
                 
-                foreach (var content in querySchedule)
+                foreach (var theSchedule in querySchedule)
                 { 
-                    
                     var ids = new List<int>();
 
-                    foreach (var user in content.Users)
+                    foreach (var user in theSchedule.Users)
                     {
                         ids.Add(user.Id);
                     }
                     
                     dataList.Add(new
                     {
-                        scheduleId = content.Id,
-                        scheduleName = content.Name,
-                        scheduleContent = content.Content,
-                        location = content.Location,
-                        startTime = content.StartTime,
-                        endTime = content.EndTime,
-                        repeatDaily = content.RepeatDaily,
-                        repeatWeekly = content.RepeatWeekly,
+                        scheduleId = theSchedule.Id,
+                        scheduleName = theSchedule.Name,
+                        scheduleContent = theSchedule.Content,
+                        location = theSchedule.Location,
+                        startTime = theSchedule.StartTime,
+                        endTime = theSchedule.EndTime,
+                        repeatDaily = theSchedule.RepeatDaily,
+                        repeatWeekly = theSchedule.RepeatWeekly,
                         participatorsId = ids
                         
                     });
@@ -251,44 +225,37 @@ namespace Backend.Biz
             }
         }
         
-        public static object GetScheduleList(object json)
+        public static object GetScheduleList(int userId)
         {
-            var body = Helper.DecodeToObject(json);
-            
             using (var context = new BackendContext())
             {
-                var scheduleId = int.Parse(body["scheduleId"].ToString());
-
-                var querySchedule = context.Schedules.Where(schedule => schedule.Id == scheduleId);
+                var queryUser = context.Users.Where(user => user.Id == userId);
+                
+                if (!queryUser.Any())
+                {
+                    return Helper.Error(404, "用户不存在");
+                }
+                
+                var querySchedule = context.Schedules.Where(schedule => schedule.OwerId == userId);
 
                 if (!querySchedule.Any())
                 {
-                    return Helper.Error(404, "日程不存在");
+                    return Helper.Error(404, "用户未创建日程");
                 }
 
-                var dataList = new List<object>();
+                var scheduleList = new List<object>();
                 
-                foreach (var content in querySchedule)
+                foreach (var schedule in querySchedule)
                 { 
                     
-                    var ids = new List<int>();
-
-                    foreach (var user in content.Users)
+                    scheduleList.Add(new
                     {
-                        ids.Add(user.Id);
-                    }
-                    
-                    dataList.Add(new
-                    {
-                        scheduleId = content.Id,
-                        scheduleName = content.Name,
-                        scheduleContent = content.Content,
-                        location = content.Location,
-                        startTime = content.StartTime,
-                        endTime = content.EndTime,
-                        repeatDaily = content.RepeatDaily,
-                        repeatWeekly = content.RepeatWeekly,
-                        participatorsId = ids
+                        scheduleId = schedule.Id,
+                        scheduleName = schedule.Name,
+                        startTime = schedule.StartTime,
+                        endTime = schedule.EndTime,
+                        repeatDaily = schedule.RepeatDaily,
+                        repeatWeekly = schedule.RepeatWeekly,
                         
                     });
                  
@@ -296,12 +263,163 @@ namespace Backend.Biz
 
                 return new
                 {
-                    dataList,
+                    scheduleList,
                     code = 200
                 };
 
             }
         }
         
+        public static object AddParticipator(object json)
+        {
+            var body = Helper.DecodeToObject(json);
+            
+            using (var context = new BackendContext())
+            {
+                var scheduleId = int.Parse(body["scheduleId"].ToString());
+                var participatorIds = JArray.Parse(body["participatorIds"].ToString());
+                var operatorId = int.Parse(body["operatorId"].ToString());
+                
+                var querySchedule = context.Schedules.Where(Schedule => Schedule.Id == scheduleId);
+                if (!querySchedule.Any())
+                {
+                    return Helper.Error(404, "日程不存在");
+                }
+                
+                var schedule = querySchedule.Single();
+                
+                //check permission
+                if (!Helper.CheckPermission(scheduleId, operatorId, true, OperationType.POST))
+                    return Helper.Error(401, "用户" + operatorId + "无操作权限");
+                
+                foreach (var participatorId in participatorIds)
+                {
+                    var id = int.Parse(participatorId.ToString());
+                    var queryUser = context.Users.Where(user => user.Id == id);
+
+                    if (!queryUser.Any())
+                    {
+                        return Helper.Error(404, "用户" + participatorId + "不存在");
+                    }
+
+                    if (schedule.Users.Contains(queryUser.Single()))
+                    {
+                        return Helper.Error(417, "参与者" + participatorId + "已存在");
+                    }
+                    
+                    schedule.Users.Add(queryUser.Single());
+                    
+                }
+                context.SaveChanges();
+                
+                var ids = new List<int>();
+                foreach (var participator in schedule.Users)
+                {
+                    ids.Add(participator.Id);
+                }
+
+                return new
+                {
+                    ids,
+                    code = 200
+                };
+
+            }
+        }
+        
+
+        public static object DeleteParticipator(object json)
+        {
+            var body = Helper.DecodeToObject(json);
+
+            using (var context = new BackendContext())
+            {
+                var scheduleId = int.Parse(body["scheduleId"].ToString());
+                var participatorIds = JArray.Parse(body["participatorIds"].ToString());
+                var operatorId = int.Parse(body["operatorId"].ToString());
+
+                var querySchedule = context.Schedules.Where(schedule => schedule.Id == scheduleId);
+                if (!querySchedule.Any())
+                    return Helper.Error(401, "删除日程不存在");
+                var theSchedule = querySchedule.Single();
+                
+                if(!Helper.CheckPermission(theSchedule.Id, operatorId, true, OperationType.DELETE))
+                    return Helper.Error(401, "用户无操作权限");
+
+                foreach (var participatorId in participatorIds)
+                {
+                    var id = int.Parse(participatorId.ToString());
+                    var queryUser = context.Users.Where(user => user.Id == id);
+
+                    if (!queryUser.Any())
+                    {
+                        return Helper.Error(404, "用户" + participatorId + "不存在");
+                    }
+                    
+                    var theUser = queryUser.Single();
+                    if (!theSchedule.Users.Contains(theUser))
+                    {
+                        return Helper.Error(404, "用户" + participatorId + "未参与该日程");
+                    }
+                    
+                    if (id == theSchedule.OwerId)
+                    {
+                        return Helper.Error(401, "无法删除日程创建者");
+                    }
+
+                    theSchedule.Users.Remove(theUser);
+
+                }
+                context.SaveChanges();
+                
+                var ids = new List<int>();
+                foreach (var user in theSchedule.Users)
+                {
+                    ids.Add(user.Id);
+                }
+
+                return new
+                {
+                    ids,
+                    code = 200
+                };
+            }
+        }
+        
+        public static object GetParticipatorList(int scheduleId)
+        {
+            using (var context = new BackendContext())
+            {
+                var querySchedule = context.Schedules.Where(schedule => schedule.Id == scheduleId);
+                
+                if (!querySchedule.Any())
+                {
+                    return Helper.Error(404, "日程不存在");
+                }
+
+                var theSchedule = querySchedule.Single();
+
+                var participatorList = new List<object>();
+                
+                foreach (var participator in theSchedule.Users)
+                { 
+                    
+                    participatorList.Add(new
+                    {
+                        participatorId = participator.Id,
+                        participatorName = participator.UserInfo.Name
+                        
+                    });
+                 
+                }
+
+                return new
+                {
+                    participatorList,
+                    code = 200
+                };
+
+            }
+        }
     }
 }
--- Controller/ScheduleController.cs
+++ Controller/ScheduleController.cs
@@ -28,9 +28,37 @@ namespace Backend.Controller
         
         [HttpGet]
         [Route("api/schedule/")]
-        public object GetSchedule(object json)
+        public object GetSchedule(int scheduleId)
         {
-            return ScheduleBiz.GetSchedule(json);
+            return ScheduleBiz.GetSchedule(scheduleId);
+        }
+        
+        [HttpGet]
+        [Route("api/schedule/list/")]
+        public object GetScheduleList(int userId)
+        {
+            return ScheduleBiz.GetScheduleList(userId);
+        }
+        
+        [HttpPost]
+        [Route("api/schedule/participator")]
+        public object AddParticipator(object json)
+        {
+            return ScheduleBiz.AddParticipator(json);
+        }
+        
+        [HttpDelete]
+        [Route("api/schedule/participator")]
+        public object DeleteParticipator(object json)
+        {
+            return ScheduleBiz.DeleteParticipator(json);
+        }
+        
+        [HttpGet]
+        [Route("api/schedule/participator/list")]
+        public object GetParticipatorList(int scheduleId)
+        {
+            return ScheduleBiz.GetParticipatorList(scheduleId);
         }
     }
 }
